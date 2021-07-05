04549b24a3abcf36c6e007f046ced24e93cb270a
MorcaDev <morca225@gmail.com>
2021-07-04 20:34:09 -0500

    loader v1 is finished (body-width is fixed)

--- script.js
+++ script.js
@@ -17,3 +17,84 @@ let desplegableSpan = desplegable.querySelector("span");
 let open = desplegable.classList.contains("visibility");
 
 desplegable.addEventListener("click", showMenu);
+
+//////////////////////////////////////////////
+
+let accountant = 0
+
+let extraWorkshops = [{
+    name: "React",
+    text: "Here You are going to learn the most popular ans useful framework to work with JS.",
+    src : "#"
+},{
+    name: "AI",
+    text: "The machine learning is the future for apps and programms , learn how to develop it and handle it.",
+    src : "#"
+},{
+    name: "Figma",
+    text: "The first step to everything is imagining, but the second is prototyping the idea.",
+    src : "#"
+},{
+    name: "HTML5",
+    text: "The website structure is an art, learn how to be the master of this art in this workshop",
+    src : "#"
+}
+]
+
+let remove = () => {
+    loaderButton.removeEventListener("click",showClasses)
+    loaderButton.remove()
+}
+
+let nodes = (number) => {
+
+    const container = document.createElement("div");
+
+    const video = document.createElement("video");
+    video.setAttribute("controls","");
+    video.setAttribute("preload","auto");
+    video.src = `${extraWorkshops[number].src}`;
+
+    const title = document.createElement("h3");
+    title.textContent= extraWorkshops[number].name 
+
+    const decoration = document.createElement("span");
+
+    const description = document.createElement("p");
+    description.textContent = extraWorkshops[number].text
+
+
+    title.appendChild(decoration);
+    container.append(video,title,description);
+
+    loaderParent.appendChild(container);
+}
+
+let showClasses = ( ev ) =>{
+        switch (accountant) {
+            case 0:
+                nodes(accountant)
+                accountant++
+                break;
+            case 1:
+                nodes(accountant)
+                accountant++
+                break;
+            case 2:
+                nodes(accountant)
+                accountant++
+                break;
+            case 3:
+                nodes(accountant)
+                remove()
+                break;
+            default:
+                break;
+        }
+}
+
+let loaderButton = document.getElementById("loader")
+let loaderParent = document.getElementById("loader-parent")
+
+loaderButton.addEventListener("click", showClasses);
+
