1322a308d41804a55dddbeb3bfd4cf532c8f8daa
MorcaDev <morca225@gmail.com>
2021-07-07 12:23:01 -0500

    zoomEvent is finished

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//////////////////////////////////////////////
+///////////////MENU DESPLEGABLE///////////////
+//////////////////////////////////////////////
+
 let showMenu = (ev) =>{ 
     if(open === false){
         desplegableMenu.classList.add("visibility");
@@ -19,66 +23,126 @@ let open = desplegable.classList.contains("visibility");
 desplegable.addEventListener("click", showMenu);
 
 //////////////////////////////////////////////
+//////////////////ZOOM///////////////////////
+//////////////////////////////////////////////
 
 let workshop1 = document.getElementById("W1");
 let workshop2 = document.getElementById("W2");
 let workshop3 = document.getElementById("W3");
 let workshop4 = document.getElementById("W4");
 
-let listOfWorkshops = [workshop1,workshop2,workshop3,workshop4]
-
-// let deleteZoom= node =>{
-//     node.classList.remove("zoom")
-//     video.classList.add("video")
-//     title.classList.add("h3")
-//     span.classList.add("span")
-//     text.classList.add("p")
-// }
+let objectW1 = {
+    div: workshop1,
+    video : workshop1.querySelector("video"),
+    title : workshop1.querySelector("h3"),
+    span : workshop1.querySelector("span"),
+    text : workshop1.querySelector("p")
+}
+let objectW2 = {
+    div: workshop2,
+    video : workshop2.querySelector("video"),
+    title : workshop2.querySelector("h3"),
+    span : workshop2.querySelector("span"),
+    text : workshop2.querySelector("p")
+}
+let objectW3 = {
+    div: workshop3,
+    video : workshop3.querySelector("video"),
+    title : workshop3.querySelector("h3"),
+    span : workshop3.querySelector("span"),
+    text : workshop3.querySelector("p")
+}
+let objectW4 = {
+    div: workshop4,
+    video : workshop4.querySelector("video"),
+    title : workshop4.querySelector("h3"),
+    span : workshop4.querySelector("span"),
+    text : workshop4.querySelector("p")
+}
 
 let closeButton = document.createElement("button")
 closeButton.classList.add("button1")
-// closeButton.addEventListener("click",deleteZoom)
 
-let zoomItself= node =>{
-    let video = node.querySelector("video")
-    video.classList.add("video")
+let closeZoom = (node,ev) =>{
+    ev.stopPropagation()
 
-    let title = node.querySelector("h3")
-    title.classList.add("h3")
+    node.video.classList.remove("video")
 
-    let span = node.querySelector("span")
-    span.classList.add("span")
+    node.title.classList.remove("h3")
 
-    let text = node.querySelector("p")
-    text.classList.add("p")
+    node.span.classList.remove("span")
 
-    node.appendChild(closeButton);
-    node.classList.add("zoom1");
+    node.text.classList.remove("p")
 
+    node.div.classList.remove("zoom1");
+
+    node.div.removeChild(closeButton);
+
+    closeButton.removeEventListener("click",indetifyZoomToClose)
+
+    node.div.addEventListener("click",identifyZoomToOpen)
 }
 
-let showZoom = (ev) =>{ 
+let indetifyZoomToClose = (ev) =>{
+    let id = ev.path[1].id 
+
+    if(id === "W1"){
+        closeZoom(objectW1,ev)
+    }else if(id === "W2"){
+        closeZoom(objectW2,ev)
+    }else if(id === "W3"){
+        closeZoom(objectW3,ev)
+    }else if(id === "W4"){
+        closeZoom(objectW4,ev)
+    }else{
+        console.log("it's not here 2")
+    }
+}
+
+let showZoom = (node) =>{
+    node.video.classList.add("video")
+
+    node.title.classList.add("h3")
+
+    node.span.classList.add("span")
+
+    node.text.classList.add("p")
+
+    node.div.classList.add("zoom1");
+
+    node.div.appendChild(closeButton);
+
+    closeButton.addEventListener("click",indetifyZoomToClose)
+    node.div.removeEventListener("click",identifyZoomToOpen)
+}
+
+let identifyZoomToOpen = (ev) =>{ 
     let idChild = ev.path[1].id 
     let idParent = ev.target.id
 
     if(idChild === "W1" || idParent === "W1"){
-        zoomItself(listOfWorkshops[0])
+        showZoom(objectW1)
     }else if(idChild === "W2" || idParent === "W2"){
-        zoomItself(listOfWorkshops[1])
+        showZoom(objectW2)
     }else if(idChild === "W3" || idParent === "W3"){
-        zoomItself(listOfWorkshops[2])
+        showZoom(objectW3)
     }else if(idChild === "W4" || idParent === "W4"){
-        zoomItself(listOfWorkshops[3])
+        showZoom(objectW4)
     }else{
-        console.log("you are in showZoom FUNCTION")
+        console.log("it's not here 1")
     }
 }
 
-listOfWorkshops.map((v)=>{
-    v.addEventListener("click",showZoom)
-})
+objectW1.div.addEventListener("click",identifyZoomToOpen)
+objectW2.div.addEventListener("click",identifyZoomToOpen)
+objectW3.div.addEventListener("click",identifyZoomToOpen)
+objectW4.div.addEventListener("click",identifyZoomToOpen)
+
 
 //////////////////////////////////////////////
+/////////////////LOADER///////////////////////
+//////////////////////////////////////////////
+
 let accountant = 0
 
 let extraWorkshops = [{
