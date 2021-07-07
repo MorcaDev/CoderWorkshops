d6b1139b53e5c84c6d87617d49fcefa95fcbd448
MorcaDev <morca225@gmail.com>
2021-07-06 19:16:22 -0500

    adding the events (but not the button listener to close ande remove)

--- script.js
+++ script.js
@@ -20,6 +20,65 @@ desplegable.addEventListener("click", showMenu);
 
 //////////////////////////////////////////////
 
+let workshop1 = document.getElementById("W1");
+let workshop2 = document.getElementById("W2");
+let workshop3 = document.getElementById("W3");
+let workshop4 = document.getElementById("W4");
+
+let listOfWorkshops = [workshop1,workshop2,workshop3,workshop4]
+
+// let deleteZoom= node =>{
+//     node.classList.remove("zoom")
+//     video.classList.add("video")
+//     title.classList.add("h3")
+//     span.classList.add("span")
+//     text.classList.add("p")
+// }
+
+let closeButton = document.createElement("button")
+closeButton.classList.add("button1")
+// closeButton.addEventListener("click",deleteZoom)
+
+let zoomItself= node =>{
+    let video = node.querySelector("video")
+    video.classList.add("video")
+
+    let title = node.querySelector("h3")
+    title.classList.add("h3")
+
+    let span = node.querySelector("span")
+    span.classList.add("span")
+
+    let text = node.querySelector("p")
+    text.classList.add("p")
+
+    node.appendChild(closeButton);
+    node.classList.add("zoom1");
+
+}
+
+let showZoom = (ev) =>{ 
+    let idChild = ev.path[1].id 
+    let idParent = ev.target.id
+
+    if(idChild === "W1" || idParent === "W1"){
+        zoomItself(listOfWorkshops[0])
+    }else if(idChild === "W2" || idParent === "W2"){
+        zoomItself(listOfWorkshops[1])
+    }else if(idChild === "W3" || idParent === "W3"){
+        zoomItself(listOfWorkshops[2])
+    }else if(idChild === "W4" || idParent === "W4"){
+        zoomItself(listOfWorkshops[3])
+    }else{
+        console.log("you are in showZoom FUNCTION")
+    }
+}
+
+listOfWorkshops.map((v)=>{
+    v.addEventListener("click",showZoom)
+})
+
+//////////////////////////////////////////////
 let accountant = 0
 
 let extraWorkshops = [{
