1322bdfea58669a133012ab7c51e29462915b50c
MorcaDev <morca225@gmail.com>
2021-07-07 13:21:22 -0500

    loader event is done with the zoom event

--- script.js
+++ script.js
@@ -94,6 +94,14 @@ let indetifyZoomToClose = (ev) =>{
         closeZoom(objectW3,ev)
     }else if(id === "W4"){
         closeZoom(objectW4,ev)
+    }else if(id === "W5"){
+        closeZoom(objectW5,ev)
+    }else if(id === "W6"){
+        closeZoom(objectW6,ev)
+    }else if(id === "W7"){
+        closeZoom(objectW7,ev)
+    }else if(id === "W8"){
+        closeZoom(objectW8,ev)
     }else{
         console.log("it's not here 2")
     }
@@ -128,6 +136,14 @@ let identifyZoomToOpen = (ev) =>{
         showZoom(objectW3)
     }else if(idChild === "W4" || idParent === "W4"){
         showZoom(objectW4)
+    }else if(idChild === "W5" || idParent === "W5"){
+        showZoom(objectW5)
+    }else if(idChild === "W6" || idParent === "W6"){
+        showZoom(objectW6)
+    }else if(idChild === "W7" || idParent === "W7"){
+        showZoom(objectW7)
+    }else if(idChild === "W8" || idParent === "W8"){
+        showZoom(objectW8)
     }else{
         console.log("it's not here 1")
     }
@@ -145,53 +161,129 @@ objectW4.div.addEventListener("click",identifyZoomToOpen)
 
 let accountant = 0
 
-let extraWorkshops = [{
+let infoExtraWorkshops = [{
     name: "React",
     text: "Here You are going to learn the most popular ans useful framework to work with JS.",
-    src : "MediaSources/videos/workshopA.mp4"
+    src : "MediaSources/videos/workshopA.mp4",
+    id  : "W5"
 },{
     name: "AI",
     text: "The machine learning is the future for apps and programms , learn how to develop it and handle it.",
-    src : "MediaSources/videos/workshopB.mp4"
+    src : "MediaSources/videos/workshopB.mp4",
+    id  : "W6"
 },{
     name: "Figma",
     text: "The first step to everything is imagining, but the second is prototyping the idea.",
-    src : "MediaSources/videos/workshopA.mp4"
+    src : "MediaSources/videos/workshopA.mp4",
+    id  : "W7"
 },{
     name: "HTML5",
     text: "The website structure is an art, learn how to be the master of this art in this workshop",
-    src : "MediaSources/videos/workshopB.mp4"
+    src : "MediaSources/videos/workshopB.mp4",
+    id  : "W8"
 }
 ]
+
+let objectW5 = {
+    div: undefined,
+    video : undefined,
+    title : undefined,
+    span : undefined,
+    text : undefined
+}
+let objectW6 = {
+    div: undefined,
+    video : undefined,
+    title : undefined,
+    span : undefined,
+    text : undefined
+}
+let objectW7 = {
+    div: undefined,
+    video : undefined,
+    title : undefined,
+    span : undefined,
+    text : undefined
+}
+let objectW8 = {
+    div: undefined,
+    video : undefined,
+    title : undefined,
+    span : undefined,
+    text : undefined
+}
 
 let remove = () => {
     loaderButton.removeEventListener("click",showClasses)
     loaderButton.remove()
 }
 
-let nodesCreator = (number) => {
-
-    const container = document.createElement("div");
-    container.classList.add("workshop")
+let nodesCreator = (number) =>{ 
+    const div = document.createElement("div");
+    div.classList.add("workshop")
+    div.id = `${infoExtraWorkshops[number].id}`
 
     const video = document.createElement("video");
     video.setAttribute("controls","");
     video.setAttribute("preload","auto");
-    video.src = `${extraWorkshops[number].src}`;
+    video.setAttribute("loop","");
+    video.classList.add("workshop__video");
+    video.src = `${infoExtraWorkshops[number].src}`;
 
-    const title = document.createElement("h3");
-    title.textContent= extraWorkshops[number].name 
+    const h3 = document.createElement("h3");
+    h3.classList.add("workshop__h3")
+    h3.textContent= infoExtraWorkshops[number].name 
 
-    const decoration = document.createElement("span");
+    const span = document.createElement("span");
 
-    const description = document.createElement("p");
-    description.textContent = extraWorkshops[number].text
+    const p = document.createElement("p");
+    p.classList.add("workshop__p")
+    p.textContent = infoExtraWorkshops[number].text  
 
+    h3.append(span)
+    div.append(video,h3,p)
 
-    title.appendChild(decoration);
-    container.append(video,title,description);
+    switch (number) {
+        case 0:
+            objectW5.div = div;
+            objectW5.video = video;
+            objectW5.title = h3;
+            objectW5.span = span;
+            objectW5.text = p;
+            objectW5.div.addEventListener("click",identifyZoomToOpen)
+            break;
+        case 1:
+            objectW6.div = div;
+            objectW6.video = video;
+            objectW6.title = h3;
+            objectW6.span = span;
+            objectW6.text = p;
+            objectW6.div.addEventListener("click",identifyZoomToOpen)
+            break;
+        case 2:
+            objectW7.div = div;
+            objectW7.video = video;
+            objectW7.title = h3;
+            objectW7.span = span;
+            objectW7.text = p;
+            objectW7.div.addEventListener("click",identifyZoomToOpen)
+            break;
+        case 3:
+            objectW8.div = div;
+            objectW8.video = video;
+            objectW8.title = h3;
+            objectW8.span = span;
+            objectW8.text = p;
+            objectW8.div.addEventListener("click",identifyZoomToOpen)
+            break;
+        default:
+            break;
+    }
 
-    loaderParent.appendChild(container);
+    h3.appendChild(span);
+    div.append(video,h3,p);
+
+    loaderParent.appendChild(div);
 
     accountant++
     
